7345bfd969f8f33081c575459cc47505eeb75f9f
Swayam Kaul <swayameron@gmail.com>
2021-12-12 11:04:51 +0530

    CSS Updted

--- Myforms.js
+++ Myforms.js
@@ -37,7 +37,7 @@ for (var i = 0; i < titlearray.length; i++) {
   var fetch = document.querySelector('.container').innerHTML;  
   dynamic.innerHTML = `<div id="cards${i}" class="boxes">
       <div class="box-content">
-        <h2>${titlearray[i]}</h2>
+        <h2 class="text">${titlearray[i]}</h2>
 
         <a class="showmore" href="http://127.0.0.1:5500/SaveResponses.html?fname=${titlearray[i]}">Save Responses</a>
    
